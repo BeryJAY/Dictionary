6ddf1b3db5f276572a000ddedb27ccc652133d7b
BeryJay Wanyange <39954324+BeryJAY@users.noreply.github.com>
2018-11-06 13:54:54 +0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+#The dict() constructor creates a dictionary and assigned to the variable "program"
+program=dict()
+#declaring the keys between 1 and 15
+for x in range (1,15):
+    program[x]=x**2
+#returning the dictionary
+print(program)
